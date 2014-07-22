1b999f6ea376923544758e8fa935800ff83763b1
rollinginsanity <dev@rollinginsanity.net>
2014-07-22 23:01:55 +1000

    Authentication now works!
    Authentication is now working. Can go to /login and enter the username
    and password defined in the dbsetup script and I log in, sessions work,
    and /logout removes the session.

--- VerseCreator.py
+++ VerseCreator.py
@@ -6,6 +6,7 @@ from flask import Flask
 from flask import g
 from flask import request
 from flask import render_template
+from flask import session, redirect, url_for
 import hashlib
 
 app = Flask(__name__)
@@ -25,18 +26,29 @@ def get_db():
 #them in.
 #TODO error throwing if a user doesn't exist.
 def authenticate_user(uname,  password_submitted):
-
+    #Getting the DB cursor
     c = get_db().cursor()
+    #Getting username from the DB. Note the [], remember this function takes a tuple, not a single string.
     c.execute('SELECT UserName, Pass FROM tblUser WHERE UserName = ? LIMIT 1',  [uname])
-    user_info =  c.fetchone()
+    #One line returned, no need for a foreach.
+    user_info = c.fetchone()
+    #Checking to see if there was any user records returned.
+    if not user_info:
+        return False
+    #Gets the password.
     password_stored = user_info[1]
-#    if password_stored == password_submitted:
-#        return "YAY!"
-    return password_stored + " " + password_submitted
+    #compares the newly hashed submitted pass against the hash in the DB
+    if password_stored == hash_pass(password_submitted):
+        return True
+    else:
+        return False
+
+    
 #This function hashes the submitted password.
 def hash_pass(PassWord):
     password_encrypted = hashlib.sha256(PassWord.encode())
     return password_encrypted.hexdigest()
+    
 #When a request is finished it also closes the database connection.
 @app.teardown_appcontext
 def close_connection(exception):
@@ -46,18 +58,32 @@ def close_connection(exception):
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    if 'username' in session:
+        return 'Hello ' + session['username']
+    else:
+        return 'Hello World!'
     
 @app.route('/login',  methods=['GET', 'POST'])
 def login():
-
+    #This will either display the login form to the users (GET method) or log in the user through args passed by the login
+    #form (POST)
     if request.method == "POST":
-        return authenticate_user(request.form['UserName'], request.form['Pass'])
+        if authenticate_user(request.form['UserName'], request.form['Pass']):
+            session['username'] = request.form['UserName']
+            return redirect(url_for('hello_world'))
+        else:
+            return "Incorrect Login Credentials"
+        
     elif request.method == "GET":
         return render_template('login.html')
+ 
+#Log the user out. 
+@app.route('/logout')
+def logout():
+    session.pop('username',  None)
+    return redirect(url_for('hello_world'))
 
-
-    
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT' #Here for demo putposes, needs to be set somewhere safer in the long term.
 
 if __name__ == '__main__':
     app.run(debug=True)
